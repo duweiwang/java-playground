12b838ed4bbcdbfadb112e88df22b2ef4718ff1e
wangduwei <wangduwei8@gmail.com>
2022-05-09 16:40:14 +0800

    完善逻辑

---
+++ src/main/java/com/wangduwei/kotlin_jvm/jvmstatic/IPlay3.java
@@ -0,0 +1,6 @@
+package com.wangduwei.kotlin_jvm.jvmstatic;
+
+public interface IPlay3 {
+
+    public void play();
+}
---
+++ src/main/java/com/wangduwei/kotlin_jvm/jvmstatic/StaticClass.java
@@ -0,0 +1,26 @@
+package com.wangduwei.kotlin_jvm.jvmstatic;
+
+public class StaticClass {
+
+
+
+    public static void test(){
+
+    }
+
+
+    static class Test{
+
+
+
+    }
+
+
+    class Innter extends StaticClass{
+
+
+
+    }
+
+
+}
